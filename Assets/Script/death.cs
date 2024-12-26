9b666cc9fbe83e0026f3c69907e32ce2c6974197
Ivan <vano19082@gmail.com>
2024-12-26 17:12:08 +0300

    1.4

---
+++ Assets/Script/death.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class death : MonoBehaviour
+{
+    public GameObject obg;
+    public int numberScenes;
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(numberScenes);
+    }
+}
