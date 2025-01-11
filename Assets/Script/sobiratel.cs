eae3d38fba38ebeb75beeb2f39bd1eaac707f682
Ivan <vano19082@gmail.com>
2025-01-11 13:38:57 +0300

    1.6

--- Assets/Script/sobiratel.cs
+++ Assets/Script/sobiratel.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class sobiratel : MonoBehaviour
 {
+    public int mushrooms = 0;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    private void OnCollisionEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
+        Debug.Log(other.gameObject.name + " " + other.gameObject.tag);
         if (other.CompareTag("Player"))
+        {
+
             Destroy(gameObject);
+            mushrooms += 1;
+            Debug.Log("Собрано" + mushrooms);
+        }
+
         // Удаляем объект, к которому прикреплен этот скрипт
 
     }
